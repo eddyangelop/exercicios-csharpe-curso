7a22146e06885216290510e805062d394a120534
eddyangelop <eddysoullive@gmail.com>
2022-09-29 18:51:49 -0300

    terceiro commit

---
+++ Exercicios_aula/EncapsulamentoManual/EncapsulamentoManual/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+namespace Encapsulamento_manual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.SetNome("T");
+
+            // (não aceita modificação do produto pelo Set por conta do metodo Get)
+            // p.Setpreco(100.00);
+
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+        }
+    }
+}
+
