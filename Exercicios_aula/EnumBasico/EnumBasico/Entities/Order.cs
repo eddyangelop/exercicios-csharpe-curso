df6ee205d9477a76c080412ca0bc8ae8f57c8f37
eddyangelop <eddysoullive@gmail.com>
2022-10-21 16:50:10 -0300

    decimo setimo

---
+++ Exercicios_aula/EnumBasico/EnumBasico/Entities/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EnumBasico.Entities.Enums;
+
+namespace EnumBasico.Entities
+{
+    internal class Order
+    {
+
+        public int Id { get; set; }
+        public DateTime Moment  { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return 
+                "Numero do cliente: "
+                + Id
+                + ", "
+                + "Data do pedido: "
+                + Moment
+                + ", "
+                + "Status de pagamento: "
+                + Status;
+        }
+
+    }
+}
---
+++ Exercicios_aula/EnumBasico/EnumBasico/Program.cs
@@ -0,0 +1,35 @@
+﻿using EnumBasico.Entities;
+using EnumBasico.Entities.Enums;
+using System;
+
+namespace enum_basico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Imprimir status em formato string");
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            Console.WriteLine("Converter strings em formato enums");
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+
+
+            Console.ReadLine();
+        }
+    }
+}
