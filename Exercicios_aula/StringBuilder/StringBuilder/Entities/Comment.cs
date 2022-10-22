d2561eab07135d94ebda2ba74df9a7acfeaab159
eddyangelop <eddysoullive@gmail.com>
2022-10-22 12:15:06 -0300

    decimo oitavo

---
+++ Exercicios_aula/StringBuilder/StringBuilder/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿namespace String_Builder.Entities
+{
+    class Comment
+    {
+        // propriedades
+        public string Text { get; set; }
+
+        // construtor padrão sem argumentos
+        public Comment()
+        {
+
+        }
+        // construtor com argumentos
+        public Comment(string text)
+        {
+            Text = text;
+        }
+
+    }
+}
