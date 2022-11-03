cca671008edceae2d87ba55f943f7d7b7d1af919
eddyangelop <eddysoullive@gmail.com>
2022-11-02 23:09:54 -0300

    vigesimo sexto

---
+++ Exercicios de fixação/ExercicioPolimorfismo/ExercicioPolimorfismo/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,26 @@
+﻿namespace exercicio_Polimorfismo.Entities
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            // 1.1 equivale a 110%
+            return base.Payment() + 1.1 * AdditionalCharge;
+
+
+        }
+    }
+}
