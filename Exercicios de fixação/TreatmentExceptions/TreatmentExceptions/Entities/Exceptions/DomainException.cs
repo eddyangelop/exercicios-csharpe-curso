469c7b678fe4f156ba36b2c39cb53abb463f846a
eddyangelop <eddysoullive@gmail.com>
2022-11-09 16:42:59 -0300

    trigesimo segundo

---
+++ Exercicios de fixação/TreatmentExceptions/TreatmentExceptions/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace TreatmentExceptions.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
