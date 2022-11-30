2471f0b10f75e71187bfd5c7cd1ef94cb804e1ac
eddyangelop <eddysoullive@gmail.com>
2022-11-30 11:25:59 -0300

    trigesimo quarto

---
+++ Exercicios aulas/BlocoUsing/BlocoUsing/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace BlocoUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Forma completa
+            /*
+            string path = @"c:\temp\filex.txt";
+
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("an error occurred");
+                Console.WriteLine(e.Message);
+            }
+            */
+
+            //Forma reduzida
+            string path = @"c:\temp\file1.txt";
+
+            try
+            {
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("an error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Exercicios aulas/FIleStreamAndStreamReader/FIleStreamAndStreamReader/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+
+namespace fileStream_streamReader
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //Forma completa.
+            /*
+            string path = @"c:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+            */
+
+            //Forma abreviada.
+            /*
+            string path = @"c:\temp\file1.txt";
+            
+            StreamReader sr = null;
+            try
+            {
+                sr = File.OpenText(path);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                
+            }
+            */
+
+            //Forma abreviada lendos todas as linhas.
+            string path = @"c:\temp\file1.txt";
+
+            StreamReader sr = null;
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+
+            }
+
+        }
+    }
+}
