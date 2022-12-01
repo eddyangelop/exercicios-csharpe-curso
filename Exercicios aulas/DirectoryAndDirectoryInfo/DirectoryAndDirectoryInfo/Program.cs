c0b9fc335776ee0337bc4b29b6ef0644a05ec446
eddyangelop <eddysoullive@gmail.com>
2022-11-30 22:12:59 -0300

    trigesimo quinto

---
+++ Exercicios aulas/DirectoryAndDirectoryInfo/DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace directory_directoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+
+            try
+            {
+                //Metodo Generic
+                //IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach(string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+                //Versão completa
+                //Directory.CreateDirectory(@"c:\temp\myfolder\newfolder");
+
+                //Versão resumida
+                Directory.CreateDirectory(path + "\\newfolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ Exercicios aulas/StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace streamWriter
+{
+    class Program
+    {
+        // cria e escreve um novo arquivo.
+        static void Main(string[] args)
+        {
+
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = new StreamWriter(targetPath))
+                {
+                    foreach (string line in lines)
+                        sw.WriteLine(line.ToUpper());
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
