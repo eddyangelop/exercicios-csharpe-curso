290629b3e938f0f1fd12c610af40b5117825dcb5
eddyangelop <eddysoullive@gmail.com>
2022-12-12 16:23:44 -0300

    quadragesimo primeiro

---
+++ Exercicios aulas/HerancaMultipla/HerancaMultipla/Devices/Printer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace HerancaMultipla.Devices
+{
+    internal class Printer : Device, IPrinter
+    {
+        public override void ProcessDoc(string document)
+        {
+            Console.WriteLine("Printer processing: " + document);
+        }
+
+        public void Print(string document)
+        {
+            Console.WriteLine("Printer print: " + document);
+        }
+    }
+}
---
+++ Exercicios aulas/HerancaMultipla/HerancaMultipla/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using HerancaMultipla.Devices;
+
+namespace HerancaMultipla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer p = new Printer() { SerialNumber = 1080 };
+            p.ProcessDoc("My letter");
+            p.Print("My letter");
+
+            Console.WriteLine();
+
+            Scanner s = new Scanner() { SerialNumber = 2003 };
+            s.ProcessDoc("My Email");
+            Console.WriteLine(s.Scan());
+
+            Console.WriteLine();
+
+            ComboDevice c = new ComboDevice() { SerialNumber = 3921 };
+            c.ProcessDoc("My dissertation");
+            c.Print("My dissertation");
+            Console.WriteLine(c.Scan());
+
+        }
+    }
+}
