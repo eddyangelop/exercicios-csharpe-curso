1910ac1f8f31118131b7760aa756c29fd905a6b7
eddyangelop <eddysoullive@gmail.com>
2022-12-14 14:24:32 -0300

    quadragesimo sexto

--- Exercicios aulas/Generics/Generics/Program.cs
+++ Exercicios aulas/Generics/Generics/Program.cs
@@ -7,16 +7,20 @@ namespace Generics
         static void Main(string[] args)
         {
 
-            PrintServiceString printService = new PrintServiceString();
+            PrintService<int> printService = new PrintService<int>();
 
             Console.Write("How many values? ");
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < n; i++)
             {
-                string x = Console.ReadLine();
+                int x = int.Parse(Console.ReadLine());
                 printService.AddValue(x);
             }
+
+            int a = (int)printService.First();
+            int b = a + 2;
+            Console.WriteLine(b);
 
             printService.Print();
             Console.WriteLine("First: " + printService.First());
