d5155943d02efd2ee197a5ed82c792162ef80d05
eddyangelop <eddysoullive@gmail.com>
2022-12-14 13:58:22 -0300

    quadragesimo quinto

--- Exercicios aulas/Generics/Generics/Program.cs
+++ Exercicios aulas/Generics/Generics/Program.cs
@@ -7,14 +7,14 @@ namespace Generics
         static void Main(string[] args)
         {
 
-            PrintService printService = new PrintService();
+            PrintServiceString printService = new PrintServiceString();
 
             Console.Write("How many values? ");
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < n; i++)
             {
-                int x = int.Parse(Console.ReadLine());
+                string x = Console.ReadLine();
                 printService.AddValue(x);
             }
 
