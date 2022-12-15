5af1e08952b4ef83fa761ce6dc7088f6f494ebdb
eddyangelop <eddysoullive@gmail.com>
2022-12-15 15:39:38 -0300

    quinquagesimo primeiro

---
+++ Exercicios aulas/GetHashCode/GetHashCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GetHashCode.Entities;
+
+
+namespace GetHashCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
