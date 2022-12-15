79d7057757747e1b3ec68b186c8cffafa180a575
eddyangelop <eddysoullive@gmail.com>
2022-12-15 15:11:46 -0300

    quinquagesimo

---
+++ Exercicios aulas/GetEquals/GetEquals/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Equals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Maria";
+            string b = "Maria";
+            string c = "João";
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(b.Equals(c));
+        }
+    }
+}
