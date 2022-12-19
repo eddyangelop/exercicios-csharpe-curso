50be188d664c6f80d110914dc21b2db1da9b269c
eddyangelop <eddysoullive@gmail.com>
2022-12-19 09:20:47 -0300

    conjuntos hashSet

---
+++ Exercicios aulas/ConjuntosHashSet/ConjuntosHashSet/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace conjuntosHashSet01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("TV"));
+            Console.WriteLine(set.Contains("Computer"));
+
+            Console.WriteLine();
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+
+
+
+        }
+    }
+}
