290f29e5d498634d1a60389a5466ae6ab0332849
eddyangelop <eddysoullive@gmail.com>
2022-12-20 17:05:01 -0300

    exercicio conjuntosDeAcessos

---
+++ Exercicios de fixação/ConjuntosAcessos/ConjuntosAcessos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using ConjuntosAcessos.Entities;
+
+namespace ConjuntosAcessos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter File full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord {UserName = name, Instant = instant });
+                        
+                    }
+                    Console.Write("Total Users: " + set.Count);
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
