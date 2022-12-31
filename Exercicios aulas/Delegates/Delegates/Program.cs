a0fd8d843ed624175eccb644be839f1789634970
eddyangelop <eddysoullive@gmail.com>
2022-12-31 10:21:57 -0300

    function Delegate

---
+++ Exercicios aulas/Delegates/Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Service.Services;
+
+
+namespace Delegates
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+            double result = op(a, b);
+
+            //Sintaxes alternativa
+            //BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+            //double result = op.Invoke(a, b);
+
+            Console.WriteLine(result);
+
+        }
+
+    }
+}
