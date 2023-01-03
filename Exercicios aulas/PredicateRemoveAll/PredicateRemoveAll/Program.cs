7edcc522862aa40e3a3901cae1d9a6dae97fb305
eddyangelop <eddysoullive@gmail.com>
2023-01-03 08:09:59 -0300

    predicate removeAll lambda

---
+++ Exercicios aulas/PredicateRemoveAll/PredicateRemoveAll/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Predicate.Entities;
+
+namespace PredicateRemoveAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("Hd Case", 80.90));
+
+            //expression Lambda
+            list.RemoveAll(p => p.Price >= 100.0);
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }           
+
+        }
+    }
+}
