1827262cf7227dcce262150c345ed72b3a7d725b
eddyangelop <eddysoullive@gmail.com>
2023-01-25 19:56:41 -0300

    EntradaSimplesComVetores2

--- Exercicios aulas/EntradaSimplesComVetores2/EntradaSimplesComVetores2/Program.cs
+++ Exercicios aulas/EntradaSimplesComVetores2/EntradaSimplesComVetores2/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Globalization;
 
-namespace entrada_com_vetores2
+namespace EntradaSimplesComVetores2
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             int n1 = int.Parse(Console.ReadLine());
             char ch = char.Parse(Console.ReadLine());
             double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
@@ -18,8 +17,6 @@ namespace entrada_com_vetores2
             int idade = int.Parse(vet[2]);
             double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
 
-
-
             Console.WriteLine("Você Digitou:");
             Console.WriteLine(n1);
             Console.WriteLine(ch);
@@ -27,9 +24,7 @@ namespace entrada_com_vetores2
             Console.WriteLine(nome);
             Console.WriteLine(sexo);
             Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
-
-            Console.ReadLine();
-
         }
+
     }
 }
