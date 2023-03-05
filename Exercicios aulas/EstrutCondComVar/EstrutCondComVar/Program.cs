bdfa79bc3dde5437e21ba65eb20eed2f449340f8
eddyangelop <eddysoullive@gmail.com>
2023-03-04 22:04:37 -0300

    estrutura condicional com variavel

---
+++ Exercicios aulas/EstrutCondComVar/EstrutCondComVar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace encadeamento_estrutura_condicional
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var numeroDoMes = int.Parse(Console.ReadLine());
+            var nomesMes = new string[]{"","Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro",
+    "Outubro", "Novembro", "Dezembro"};
+
+            if (numeroDoMes != 0)
+            {
+                Console.WriteLine(nomesMes[numeroDoMes]);
+            }
+
+        }
+
+    }
+
+
+}
