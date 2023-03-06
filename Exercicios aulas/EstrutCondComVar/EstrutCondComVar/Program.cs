5a92d9c718d983ee13445c50b8a80ee5bc27369f
eddyangelop <eddysoullive@gmail.com>
2023-03-05 21:52:07 -0300

    estrutura condicional com variavel refatorada

--- Exercicios aulas/EstrutCondComVar/EstrutCondComVar/Program.cs
+++ Exercicios aulas/EstrutCondComVar/EstrutCondComVar/Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
-namespace encadeamento_estrutura_condicional
+namespace EstruturaCondicionalVar
 {
     class program
     {
         static void Main(string[] args)
         {
             var numeroDoMes = int.Parse(Console.ReadLine());
-            var nomesMes = new string[]{"","Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro",
-    "Outubro", "Novembro", "Dezembro"};
+            var nomesMes = new string[] { "","Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
 
-            if (numeroDoMes != 0)
+            if (numeroDoMes <= 0 || numeroDoMes > 12)
             {
-                Console.WriteLine(nomesMes[numeroDoMes]);
+                Console.WriteLine("Mês Invalido!");
+            }
+            else
+            {
+                Console.WriteLine("Seu Mês é: " + nomesMes[numeroDoMes]);
             }
 
         }
