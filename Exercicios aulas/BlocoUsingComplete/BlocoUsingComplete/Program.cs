b1d542f8513c1522f44d0f49de240d5f0ce9054f
eddyangelop <eddysoullive@gmail.com>
2023-03-10 23:48:03 -0300

    bloco using complete

---
+++ Exercicios aulas/BlocoUsingComplete/BlocoUsingComplete/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BlocoUsingComplete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\filex.txt";
+
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("an error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
