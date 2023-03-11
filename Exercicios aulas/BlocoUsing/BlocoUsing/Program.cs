721d0d1d6ec2e3d3c8fe8d38a90f55757a0cb938
eddyangelop <eddysoullive@gmail.com>
2023-03-10 23:45:13 -0300

    bloco using basic

--- Exercicios aulas/BlocoUsing/BlocoUsing/Program.cs
+++ Exercicios aulas/BlocoUsing/BlocoUsing/Program.cs
@@ -1,37 +1,11 @@
 ﻿using System;
 
-namespace BlocoUsing
+namespace BlocoUsingBasic
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Forma completa
-            /*
-            string path = @"c:\temp\filex.txt";
-
-            try
-            {
-                using (FileStream fs = new FileStream(path, FileMode.Open))
-                {
-                    using (StreamReader sr = new StreamReader(fs))
-                    {
-                        while (!sr.EndOfStream)
-                        {
-                            string line = sr.ReadLine();
-                            Console.WriteLine(line);
-                        }
-                    }
-                }
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("an error occurred");
-                Console.WriteLine(e.Message);
-            }
-            */
-
-            //Forma reduzida
             string path = @"c:\temp\file1.txt";
 
             try
